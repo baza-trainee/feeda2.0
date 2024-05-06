b0c30cdaa1942e707e23d52184e75355e1be7d04
Matviy <antipin.m9971@gmail.com>
2024-05-06 21:54:46 +0300

    feat(create header/create global container): create header/ create global container

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return <>
   <Routes>
+    <Route path="/" element={<Layout/>}>
+
     <Route path="/login" element={<LoginPage/>}/>
     <Route path="/register" element={<RegisterPage/>}/>
     <Route path="/forgot-pass" element={<RegisterPage/>}/>
+    </Route>
   </Routes>
   </>
 }
---
+++ src/components/UI/Header/Header.styled.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const HeaderStyled = styled.header`
+	padding-left: 32px;
+`
+
+export const HeaderLogo = styled.h1`
+	width: 286px;
+	border-radius: 0px 0px 4px 4px;
+	background: var(--yellow);
+	padding: 16px 24px;
+	color: var(--black);
+	font-family: "Exo 2";
+	font-size: var(--title-main-medium-fs);
+	font-weight: var(--title-main-medium-fw);
+	line-height: var(--title-main-medium-lh);
+	letter-spacing: 0%;
+`
