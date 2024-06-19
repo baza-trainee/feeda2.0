bd15e3c6a1719a30daca3f9ea58c0bea42a2359b
VladAntonov95 <vladislav.95antonov@gmail.com>
2024-06-19 23:44:42 +0300

    fix/pages: fix pages position

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,16 +2,19 @@ import React, { Suspense } from 'react';
 import Header from '../UI/Header/Header';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../UI/Sidebar/Sidebar';
+import { PageSection } from '../../pages/AddPage/AddPage.styled';
 
 const Layout = () => {
   return (
     <>
       <div>
         <Header />
-        <Sidebar />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Outlet />
-        </Suspense>
+        <PageSection>
+          <Sidebar />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Outlet />
+          </Suspense>
+        </PageSection>
       </div>
     </>
   );
